3b43321507179d1a20905ddb9d717c49e94225a6
Venu Sai <venusaim23@gmail.com>
2021-10-09 18:26:36 +0530

    migrate to view binding

--- app/src/main/java/com/v0id/taskphorse/MainActivity.java
+++ app/src/main/java/com/v0id/taskphorse/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.v0id.taskphorse.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
